113701c58c8e4dec5d0e4bc4927f0b0b073ef319
rorolamenace <rorolamenace@users.noreply.github.com>
2024-03-22 00:31:20 +0100

    Seeding sample data to MongoDB

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
