09eaf41d4ef35a87143414ad5a647a36752e48a7
rorolamenace <rorolamenace@users.noreply.github.com>
2024-03-22 09:28:15 +0100

    Get Products From Database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js'
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB.
@@ -13,15 +13,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  //res.header("Access-Control-Allow-Origin", "*");
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  //res.header("Access-Control-Allow-Origin", "*");
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
