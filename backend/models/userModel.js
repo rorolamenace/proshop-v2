4766c2982d6e90f8cf3c0033721d3c39f53ec00c
rorolamenace <rorolamenace@users.noreply.github.com>
2024-03-25 16:07:48 +0100

    User Profile Endpoints

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Hash the password before saving.
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("user", userSchema);
 
 export default User;
