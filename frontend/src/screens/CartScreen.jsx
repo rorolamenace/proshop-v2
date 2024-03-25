0a43703c83de954ca87ac9ec3d043dc378773cd3
rorolamenace <rorolamenace@users.noreply.github.com>
2024-03-25 12:31:03 +0100

    Remove from Cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
-import { addToCart } from "../slices/cartSlide";
+import { addToCart, removeFromCart } from "../slices/cartSlide";
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -20,8 +20,14 @@ const CartScreen = () => {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
-  const addToCartHandler = (product, qty) => {
+  const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
+  };
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
   };
 
   return (
@@ -60,7 +66,11 @@ const CartScreen = () => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                    <Button type="button" variant="light">
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
                       <FaTrash />
                     </Button>
                   </Col>
@@ -86,6 +96,7 @@ const CartScreen = () => {
                             type="button"
                             className="btn-block"
                             disable={cartItems.length === 0}
+                            onClick={checkoutHandler}
                           >
                             Proceed to Checkout
                           </Button>
